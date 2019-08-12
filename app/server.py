a3be2da766a1eaed0cf3870022be403c227db8d8
alex kelly <kelster101757@hotmail.com>
2019-08-12 20:45:29 +0100

    changed to use my skip files

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/14xXaDqj9aTTkSuRpLl68bb1UnY6bqZQF?alt=media&key=AIzaSyCs11BnCZwCIiGNiZ88JWTIJ2WsdRmqZR4'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['skips','skips_full']
 path = Path(__file__).parent
 
 app = Starlette()
